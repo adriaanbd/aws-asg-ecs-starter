bd0fa37bc7362ecb60b1622e4125900e655f6d46
Adriaan Beiertz <adriaanbd@gmail.com>
2020-11-04 16:38:33 -0500

    deleted vpc_id & key_name output

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,3 @@
-output "vpc_id" {
-  description = "The ID of the Main VPC"
-  value       = module.network.vpc_id
-}
-
 output "bastion_a_ip" {
   description = "The Public IP of the Instance A"
   value       = module.compute.bastion_a_ip
@@ -11,10 +6,6 @@ output "bastion_a_ip" {
 output "app_a_ip" {
   description = "The Public IP of the Instance A"
   value       = module.compute.app_a_ip
-}
-
-output "ssh_key_name" {
-  value = module.security.ssh_key_name
 }
 
 output "ssh_key_path" {
