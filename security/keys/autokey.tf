4478f63154710ad9d83a71148a6d87718102a952
Adriaan Beiertz <adriaanbd@gmail.com>
2020-11-04 01:20:48 -0500

    added security corrections

--- security/keys/autokey.tf
+++ security/keys/autokey.tf
@@ -4,11 +4,11 @@ resource "tls_private_key" "rsa_key" {
 }
 
 resource "aws_key_pair" "key_pair" {
-  key_name   = var.key_name
+  key_name   = var.ssh_key_name
   public_key = tls_private_key.rsa_key.public_key_openssh
 }
 
 resource "local_file" "my_key_file" {
   content     = tls_private_key.rsa_key.private_key_pem
-  filename    = pathexpand("~/.ssh/${var.key_pair_name}.pem")
+  filename    = pathexpand("~/.ssh/${var.ssh_key_name}.pem")
 }
--- security/variables.tf
+++ security/variables.tf
@@ -1,3 +1,11 @@
+variable "namespace" {
+  type = string
+}
+
+variable "vpc_id" {
+  type = string
+}
+
 variable "ssh_key_name" {
   type        = string
   description = "Name of the generated AWS Key Pair"
