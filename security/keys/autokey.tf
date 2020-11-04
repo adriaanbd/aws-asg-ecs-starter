2e3655d52a89997b30d8347d2937a58d87dae23d
Adriaan Beiertz <adriaanbd@gmail.com>
2020-11-04 13:00:01 -0500

    automated chmod 400 on ssh key

--- security/keys/autokey.tf
+++ security/keys/autokey.tf
@@ -11,4 +11,8 @@ resource "aws_key_pair" "key_pair" {
 resource "local_file" "my_key_file" {
   content     = tls_private_key.rsa_key.private_key_pem
   filename    = pathexpand("~/.ssh/${var.ssh_key_name}.pem")
+
+  provisioner "local-exec" {
+    command = "chmod 400 ${local_file.my_key_file.filename}"
+  }
 }
