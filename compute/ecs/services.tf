cff46d0bf49763e57a6344b61e6afa1f9bfa0f3e
Adriaan Beiertz <adriaanbd@gmail.com>
2020-11-05 14:04:06 -0500

    updated min health instance to 50

--- compute/ecs/services.tf
+++ compute/ecs/services.tf
@@ -2,15 +2,15 @@
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/ecs_service
 resource "aws_ecs_service" "ecs_service" {
   name                               = var.service_name
-  # iam_role                         = var.service_role
+  # iam_role                         = var.service_role  # this is for Load Balancing
   cluster                            = aws_ecs_cluster.cluster.id
-  deployment_minimum_healthy_percent = 0
+  deployment_minimum_healthy_percent = 50
   deployment_maximum_percent         = 100
   task_definition                    = aws_ecs_task_definition.task.family
   launch_type                        = "EC2"                      # default
   scheduling_strategy                = "REPLICA"                  # default
   desired_count                      = var.desired_service_count  # required if REPLICA
   deployment_controller {
-    type = "ECS" # default
+    type = "ECS"                                                  # default
   }
 }
