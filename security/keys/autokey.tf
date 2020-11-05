b320b3ba4b1e0fb06f7f91b1094541c93a57611c
Adriaan Beiertz <adriaanbd@gmail.com>
2020-11-05 12:40:50 -0500

    added local variables for bash or powershell command

--- security/keys/autokey.tf
+++ security/keys/autokey.tf
@@ -1,3 +1,13 @@
+locals {
+  is_windows = substr(pathexpand("~"), 0, 1) == "/" ? false : true
+  key_file   = pathexpand("~/.ssh/${var.ssh_key_name}.pem")
+}
+
+locals {
+  bash       = "chmod 400 ${local.key_file}"
+  powershell = "icacls ${local.key_file} /inheritancelevel:r /grant:r ${var.os_username}:R"
+}
+
 resource "tls_private_key" "rsa_key" {
   algorithm = "RSA"
   rsa_bits  = 4096
@@ -10,16 +20,9 @@ resource "aws_key_pair" "key_pair" {
 
 resource "local_file" "my_key_file" {
   content     = tls_private_key.rsa_key.private_key_pem
-  filename    = pathexpand("~/.ssh/${var.ssh_key_name}.pem")
+  filename    = local.key_file
 
-
-  # ### BASH ###
-  # provisioner "local-exec" {
-  #  command = "chmod 400 ${local_file.my_key_file.filename}"
-  # }
-
-  ### WINDOWS ###
   provisioner "local-exec" {
-    command = "icacls ${local_file.my_key_file.filename} /inheritancelevel:r /grant:r ${var.os_username}:R"
+    command = local.is_windows ? local.powershell : local.bash
   }
 }
