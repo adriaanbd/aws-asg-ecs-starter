38146c98385e96fe97fbc765bb6458e585dd8d0f
Adriaan Beiertz <adriaanbd@gmail.com>
2020-11-05 14:02:12 -0500

    disabled dummy app instances and related outputs

--- compute/instances/outputs.tf
+++ compute/instances/outputs.tf
@@ -2,6 +2,6 @@ output "bastion_a_ip" {
   value = aws_instance.bastion_a.public_ip
 }
 
-output "app_a_ip" {
-  value = aws_instance.app_a.private_ip
-}
+# output "app_a_ip" {
+#   value = aws_instance.app_a.private_ip
+# }
--- outputs.tf
+++ outputs.tf
@@ -3,10 +3,10 @@ output "bastion_a_ip" {
   value       = module.compute.bastion_a_ip
 }
 
-output "app_a_ip" {
-  description = "The Public IP of the Instance A"
-  value       = module.compute.app_a_ip
-}
+# output "app_a_ip" {
+#   description = "The Public IP of the Instance A"
+#   value       = module.compute.app_a_ip
+# }
 
 output "ssh_key_path" {
   value = module.security.ssh_key_path
