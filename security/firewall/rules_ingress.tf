6a568c22878d38c61d64a91f5070b6ec66330ae2
Adriaan Beiertz <adriaanbd@gmail.com>
2020-11-05 02:16:02 -0500

    ecs and asg working in pub sub

--- security/firewall/rules_ingress.tf
+++ security/firewall/rules_ingress.tf
@@ -7,24 +7,6 @@ resource "aws_security_group_rule" "ssh_in_bastion" {
   security_group_id = aws_security_group.bastion_sg.id
 }
 
-resource "aws_security_group_rule" "http_in_bastion" {
-  type              = "ingress"
-  from_port         = 80
-  to_port           = 80
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.bastion_sg.id
-}
-
-resource "aws_security_group_rule" "https_in_bastion" {
-  type              = "ingress"
-  from_port         = 443
-  to_port           = 443
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.bastion_sg.id
-}
-
 resource "aws_security_group_rule" "ssh_in_app" {
   type                     = "ingress"
   from_port                = 22
@@ -32,4 +14,14 @@ resource "aws_security_group_rule" "ssh_in_app" {
   protocol                 = "tcp"
   security_group_id        = aws_security_group.app_sg.id
   source_security_group_id = aws_security_group.bastion_sg.id
+}
+
+resource "aws_security_group_rule" "all_tcp_in_bastion" {
+  type              = "ingress"
+  description       = "WARNING: Allow all TCP ingress traffic"
+  from_port         = 0
+  to_port           = 65535
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.bastion_sg.id
 }
