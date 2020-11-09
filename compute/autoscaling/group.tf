f445a9153a64d35f0fcaa4cfcf64e2d287a79808
Adriaan Beiertz <adriaanbd@gmail.com>
2020-11-09 14:39:41 -0500

    corrected comment indent on asg group

--- compute/autoscaling/group.tf
+++ compute/autoscaling/group.tf
@@ -5,7 +5,7 @@ resource "aws_autoscaling_group" "asg_for_ecs" {
   desired_capacity          = var.asg_desired   # optional
   launch_configuration      = aws_launch_configuration.launch_config.name
   vpc_zone_identifier       = [var.prv_sub_a_id, var.prv_sub_b_id]
-  health_check_grace_period = 30            # default is 300
+  health_check_grace_period = 30                # default is 300
   # https://stackoverflow.com/questions/42466157/whats-the-difference-between-elb-health-check-and-ec2-health-check
   # health_check_type     = "ELB"  # default is EC2
 }
